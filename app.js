69540fda3fc9f757077ce201f2761a437f483bf5
Brian Borealis <brian.borealis@gmail.com>
2015-10-02 08:54:50 -0700

    add header.html and home.html and update app routes

---
+++ app.js
@@ -0,0 +1,29 @@
+var nicksFlicks = angular.module('nicksFlicks', ['ui.router']);
+
+nicksFlicks.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('home', {
+    url:"",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: 'partials/home.html',
+      },
+    }
+  });
+
+  $stateProvider.state('reviews', {
+    url:"",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: 'partials/home.html',
+      },
+    }
+  });
+  
+});
