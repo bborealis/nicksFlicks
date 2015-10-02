8729d955acd7282fc7916310b3cfae42393303f3
Brian Borealis <brian.borealis@gmail.com>
2015-10-02 12:34:48 -0700

    got movie reviews to display but not toggling correctly

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -1,4 +1,18 @@
 nicksFlicks.controller('ReviewsCtrl', function ReviewersCtrl($scope, ReviewsFactory) {
   $scope.reviews = ReviewsFactory.reviews;
   $scope.ReviewsFactory = ReviewsFactory;
+
+
+
+  $scope.selectReview = function(review) {
+    review.selected = !review.selected;
+
+    console.log(review.selected);
+  };
+
+  $scope.unselectReview = function(review) {
+    review.selected = false;
+    console.log(review.selected);
+  };
+
 });
